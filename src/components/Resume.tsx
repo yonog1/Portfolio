f3edd92e4d47e004b7367461e037f77a1cffd3fc
yonatan goldin <yonatangoldin69@gmail.com>
2023-02-17 20:48:23 +0200

    some dummy data to share layout

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -4,86 +4,63 @@ import React from "react";
 export default function Resume() {
     return (
         <>
-            <Grid container padding={"1vh"} spacing={2}>
-                <Grid item xs={3}>
-                    <Typography variant="h5">Objective</Typography>
+            <Grid container padding={"4vh"} spacing={2}>
+                <Grid container xs={3}>
+                    <Typography variant="h5">Lorem</Typography>
                     <Typography variant="body1">
-                        Self-taught software developer with a proven ability to
-                        adapt to both self-starting and collaborative
-                        environments while staying focused on achieving
-                        high-quality results under strict deadlines. Autodidact,
-                        and passionate about programming, looking for
-                        opportunities to grow and learn as a developer.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Repudiandae, tenetur officia quidem modi quae iste magni
+                        ipsam, debitis optio quas amet nam quod voluptates
+                        dignissimos. Iure ea totam explicabo inventore?
                     </Typography>
                 </Grid>
 
-                <Grid item xs={3}>
-                    <Typography variant="h5">Certifications</Typography>
+                <Grid item xs={3} justifyContent="flex-end">
+                    <Typography variant="h5">Lorem1</Typography>
                     <List>
                         <Typography variant="body1">
                             <ListItem>
-                                CCNA Routing and Switching: Introduction to
-                                Networks
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Soluta iusto perspiciatis
+                                quisquam, consequuntur voluptatem animi quis
                             </ListItem>
                             <ListItem>
-                                Intro to Computer Science - University of Tel
-                                Aviv
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Soluta iusto perspiciatis
+                                quisquam, consequuntur voluptatem animi quis
                             </ListItem>
                         </Typography>
                     </List>
                 </Grid>
 
-                <Grid item xs={3}>
+                <Grid container xs={3}>
                     <List>
-                        <Typography variant="h5">Skills</Typography>
-                        <ListItem>Javascript / React / Node</ListItem>
-                        <ListItem>HTML / CSS</ListItem>
-                        <ListItem>Python</ListItem>
-                        <ListItem>C# / ASP.NET</ListItem>
-                        <ListItem>Computer Networking</ListItem>
-                        <ListItem>
-                            Multilingual {"(English, Spanish, Hebrew)"}
-                        </ListItem>
+                        <Typography variant="h5">Lorem2</Typography>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
                     </List>
                 </Grid>
 
-                <Grid item xs={6}>
+                <Grid container xs={6} marginTop="15vh">
                     <List>
-                        <Typography variant="h5">Experience</Typography>
+                        <Typography variant="h5">Lorem3</Typography>
                         <Typography variant="h6">
-                            System Administrator | IDF - Technological Unit |
-                            03/2021 - Now
+                            Lorem ipsum dolor sit amet
                         </Typography>
-                        <ListItem>
-                            Manage user accounts, security groups and
-                            permissions to limit information access to only
-                            those who are authorized to view and/or edit it.
-                        </ListItem>
-                        <ListItem>
-                            Maintained and developed network infrastructure
-                            while managing networking hardware.
-                        </ListItem>
-                        <ListItem>
-                            Created automated solutionsfor end users to
-                            facilitate their workflow.
-                        </ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
 
                         <Typography variant="h6">
-                            Computer Lab Supervisor | Appleseeds | 01/2018 -
-                            02/2020
+                            Lorem ipsum dolor sit amet
                         </Typography>
-                        <ListItem>
-                            Troubleshot computer hardware and gave adequate
-                            solutions to fix/overcome the issues.
-                        </ListItem>
-                        <ListItem>
-                            Trained 5 junior techs to manage tickets, take care
-                            of daily workload and communicate with customers.
-                        </ListItem>
-                        <ListItem>
-                            Talked with suppliers to purchase hardware
-                            components for replacements and lab equipment.
-                        </ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                        <ListItem>Lorem ipsum dolor sit amet</ListItem>
                     </List>
                 </Grid>
             </Grid>
